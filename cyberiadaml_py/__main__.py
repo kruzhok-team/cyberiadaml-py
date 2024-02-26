f2fb6b167717f89898b83463c10f2cae29545ca2
L140-beep <kartoshka0302@mail.ru>
2024-02-26 13:50:52 +0800

    delete __main__

--- cyberiadaml_py/__main__.py
+++
@@ -1,7 +0,0 @@
-from cyberiadaml_py.cyberiadaml_builder import CGMLBuilder
-from cyberiadaml_py.cyberiadaml_parser import CGMLParser
-
-if __name__ == '__main__':
-    with open('demos/CyberiadaFormat-Blinker.graphml', 'r') as f:
-        a = CGMLParser().parseCGML(f.read())
-        print(CGMLBuilder().build(a))
