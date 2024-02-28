0d011c3019cc3800ae53fd35e61cfc3a60ba84f4
L140-beep <kartoshka0302@mail.ru>
2024-02-28 06:43:32 +0300

    WIP: doc elements

--- cyberiadaml_py/types/elements.py
+++ cyberiadaml_py/types/elements.py
@@ -1,3 +1,4 @@
+"""Module containing the types of schema elements."""
 from typing import (
     List,
     Dict,
@@ -37,6 +38,16 @@ class CGMLComponent:
 
 @dataclass
 class CGMLInitialState:
+    """
+    Data class with information about initial state (pseudo node).
+
+    Intiial state is <node>, that contains data node with key 'dInitial'.
+    id: state's id.
+    target: state's id, thats connetcted with initial state\
+        (<edge source="initial state id" target="target state's id">)
+    position: x, y properties of data node with 'dGeometry' key.
+    """
+
     id: str
     target: str
     position: Optional[Point] = None
@@ -44,6 +55,18 @@ class CGMLInitialState:
 
 @dataclass
 class CGMLTransition:
+    """
+    Data class with information about transition(<edge>).
+
+    source: <edge> source property's content.
+    target: <edge> target property's content.
+    actions: content of data node with 'dData' key.
+    color: content of data node with 'dColor' key.
+    position: x, y properties of data node with 'dGeometry' key.
+    unknownDatanodes: all datanodes, whose information\
+    is not included in the type.
+    """
+
     source: str
     target: str
     actions: str
@@ -54,6 +77,14 @@ class CGMLTransition:
 
 @dataclass
 class CGMLNote:
+    """
+    Dataclass with infromation about note.
+
+    Note is <node> containing data node with key 'dNote'
+    unknownDatanodes: all datanodes, whose information\
+        is not included in the type.
+    """
+
     position: Point
     text: str
     unknownDatanodes: List[CGMLDataNode]
@@ -62,7 +93,25 @@ class CGMLNote:
 
 @dataclass
 class CGMLElements:
-    """Blabla."""
+    """
+    Dataclass with elements of parsed scheme.
+
+    Contains dict of states, where the key is state's id.
+    Also contains trainstions, components, awaialable keys, notes.
+
+    meta: content of data node\
+        with key 'dData' inside <node id="">
+
+    format: content of data node with key 'gFormat'.
+
+    platform: content of data node with key 'dName'\
+        inside <node id="">
+
+    States doesn't contains components nodes and initial state.
+
+    Transitions doesn't contains edges from meta-node(with id='')\
+        to components nodes.
+    """
 
     states: Dict[str, CGMLState]
     transitions: List[CGMLTransition]
